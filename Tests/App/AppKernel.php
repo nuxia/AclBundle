71bf3f73adfa0f5b65fe29b3c15810bd4dc9a5b4
Alexandre Dupuy <alex.dpy@gmail.com>
2015-09-11 00:46:02 +0200

    TravisCI : mysql, pgsql and sqlite test environments

--- Tests/App/AppKernel.php
+++ Tests/App/AppKernel.php
@@ -20,5 +20,31 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config.yml');
+        $loader->load(function (\Symfony\Component\DependencyInjection\ContainerBuilder $container) {
+            if (!isset($GLOBALS['db_type']) || 'pdo_sqlite' === $GLOBALS['db_type']) {
+                $connection = array(
+                    'driver' => 'pdo_sqlite',
+                    'memory' => true,
+                );
+            } else {
+                $connection = array(
+                    'driver' => $GLOBALS['db_type'],
+                    'host' => $GLOBALS['db_host'],
+                    'port' => $GLOBALS['db_port'],
+                    'dbname' => $GLOBALS['db_name'],
+                    'user' => $GLOBALS['db_username'],
+                    'password' => $GLOBALS['db_password'],
+                );
+            }
+
+            $container->loadFromExtension('doctrine', array(
+                'dbal' => array(
+                    'default_connection' => 'default',
+                    'connections' => array(
+                        'default' => $connection
+                    )
+                )
+            ));
+        });
     }
 }
